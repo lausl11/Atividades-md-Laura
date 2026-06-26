20fd2084fe9ee82c6a0225b08030f6037aa38a1b
lausl11 <laura.laurindo@escola.pr.gov.br>
2026-06-26 10:39:27 -0300

    ativ-10

---
+++ ativ-10/node.js
@@ -0,0 +1,19 @@
+console.log('1-o gcep é um lixo')
+
+console.log('2-O site do Gcep')
+
+console.log('3-o victor é autista')
+
+console.log('4-eu odeio banana')
+
+console.log('5-eu to com fome 90% do dia, e quando nao estou é pq to dormindo')
+
+console.log('6-Bem vindo ao site do GCEP! aqui vc vai sofrer programandooooo :)')
+
+console.log('7-eu to com fome 90% do dia, e quando nao estou é pq to dormindo')
+
+console.log("========= Teste Simples ==========");
+console.log("Mensagem 1: todos os adolencente deveriam ser clt");
+console.log("Mensagem 2: feliz natal");
+console.log("Teste finalizado com sucesso!");
+
