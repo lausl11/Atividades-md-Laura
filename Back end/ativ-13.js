f6249486c298cbb3ea638b869f432f84616957c6
lausl11 <laura.laurindo@escola.pr.gov.br>
2026-07-03 12:08:12 -0300

    ativ-13

---
+++ Back end/ativ-13.js
@@ -0,0 +1,73 @@
+const ususarios = [
+    {
+        id:1,
+        nome: 'victor',
+        email: 'victor@escola.com',
+        perfil: 'aluna'
+    },
+    {
+        id:2,
+        nome: 'laura',
+        email: 'laura@escola.com',
+        perfil: 'aluna'
+    },
+    {
+        id:3,
+        nome: 'michael',
+        email: 'michael@escola.com',
+        perfil: 'professor'
+    },
+]
+
+const avisos = [
+    {
+        titulo: 'queremos comer',
+        descrição: 'os alunos de cep estao morrendo de fome'
+    },
+
+    {
+        titulo: 'testando o professor',
+        descrição: 'se voce realemente le, na nossa proxima aula, fala pra mim "pato"'
+    },
+
+    {
+        titulo: 'perigo na escola',
+        descrição: 'alunso desconhecidos estao proclamando a volta do neo-nascismo atraves de perfis anonimos'
+    },
+
+    {
+        titulo: 'frango',
+        descrição: 'eu como todo os dias'
+    },
+]
+const tarefas = [
+    {
+        tarefa: 'limpar minha moto',
+        status: 'nao realizada'
+    },
+
+    {
+        tarefa:'terminar os excercicios do maidel',
+        status: 'nao realizada' 
+    },
+
+    {
+        tarefa: 'comer e me sentir mal',
+        status: 'feito com sucesso'
+    },
+]
+const reservas = [
+    {
+        local: 'lab 318',
+        dia: '1/07',
+        status: 'concluida'
+    },
+
+    {
+        local: 'quadra',
+        dia: '2/07',
+        status: 'nao aceita '
+    }
+]
+
+console.log (ususarios, tarefas, reservas, avisos)
