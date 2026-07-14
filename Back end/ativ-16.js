db7bf606add3a66bfb82a8118c44a8e10567576d
lau.sl11 <laura.laurindo@Escola.pr.gov.br>
2026-07-14 07:55:51 -0300

    ativ-16

---
+++ Back end/ativ-16.js
@@ -0,0 +1,15 @@
+// avisos/
+// |
+// |___ servidor/     
+// |       |
+// |       |__rotas/
+// |       |__dados/
+// |       |__app.js/
+// |
+// |___ front-end/
+// |       |
+// |       |__index.html
+// |       |__style.css
+// |       |__script.js
+
+// 4- o login fica no front end, e o servidor na aba servidor   
